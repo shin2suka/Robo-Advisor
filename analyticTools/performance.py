0b7577b785b092444fe454c5ccb4a03b48811f39
Zale-1516 <65134434+Zale-1516@users.noreply.github.com>
2020-07-03 23:46:48 -0400

    update MVRR

--- analyticTools/performance.py
+++ analyticTools/performance.py
@@ -32,14 +32,14 @@ def get_performance(returns, risk_free):
     stats["maxDD"] = round(maxdrawdown(returns), 5)
     return stats
 
-def get_MW_performance(portfolio_value, injective_capital, injection_cycle, total_investment_period, injection_dates):
+def get_MW_performance(portfolio_value, injective_capital, injection_cycle, injection_dates):
     # use numpy.irr calculate the IRR which is the get_MW_performance
     # outflow - inflow = 0
     # -100,000 - sum(10,000/(1 + IRR)**i) + end_value/(1 + IRR)**n
     # if injection_cycle == 6 months, IRR is compounding half-yearly
     initial_outflow = portfolio_value[0]
     ending_inflow = portfolio_value[-1]
-    
+
     cash_flow = [-initial_outflow]
     cash_flow.extend([-injective_capital] * len(injection_dates))
     cash_flow.append(ending_inflow)
