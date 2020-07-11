74cae6828ca8a47216518a4993b09dde0143b11c
Zale-1516 <65134434+Zale-1516@users.noreply.github.com>
2020-07-11 17:58:09 -0400

    update performance

--- analyticTools/performance.py
+++ analyticTools/performance.py
@@ -1,49 +1,77 @@
 import numpy as np
 from scipy.stats import skew
+import pandas as pd
 
-
-def maxdrawdown(timeseries, start=0, end=None):
-    if end:
-        end = len(timeseries)
-    i = np.argmax(np.maximum.accumulate(timeseries[start:end]) - timeseries[start:end]) # end of the period
+def maxdrawdown(timeseries):
+    i = np.argmax(np.maximum.accumulate(timeseries) - timeseries) # end of the period
     if i == 0:
         return 0
-    j = np.argmax(timeseries[start:end][:i]) # start of period
-    return timeseries[start:end][i] / timeseries[start:end][j] - 1
+    j = np.argmax(timeseries[:i]) # start of period
+    return timeseries[i] / timeseries[j] - 1
 
 
-def get_performance(returns, risk_free):
+# def get_TW_performance(returns, risk_free = None):
+#
+#     def CAGR(x):
+#         return np.cumprod(1 + x)[-1]**(252/len(x)) - 1
+#
+#     price = np.cumprod(1+returns)
+#     print(price)
+#     risk_free = (risk_free.groupby(risk_free.index.year).mean()).values
+#     returns = ((1 + returns).groupby(returns.index.year).prod() - 1).values
+#     print(returns)
+#     assert len(returns) == len(risk_free)
+#
+#     stats = {}
+#     stats["CAGR"] = round(CAGR(returns), 5)
+#     stats["vol"] = round(np.std(returns)*np.sqrt(12), 5)
+#     stats["Sharpe"] = round(CAGR(returns) / stats["vol"], 5)
+#     stats["skew"] = round(float(skew(returns)), 5)
+#     stats["maxDD"] = round(maxdrawdown(returns), 5)
+#     return stats
 
-    def CAGR(x):
-        return np.cumprod(1 + x)[-1]**(252/len(x)) - 1
+def get_performance(return_value, acc_value, injection_dates, before_injection_dates, Time_weighted = True):
 
-    price = np.cumprod(1+returns)
-    print(price)
-    risk_free = (risk_free.groupby(risk_free.index.year).mean()).values
-    returns = ((1 + returns).groupby(returns.index.year).prod() - 1).values
-
-    assert len(returns) == len(risk_free)
+    portfolio_weights = get_weights(acc_value, injection_dates, before_injection_dates, Time_weighted)
+    index, returns = get_returns(acc_value, injection_dates, before_injection_dates, portfolio_weights)
+    std = get_vol(return_value, index, portfolio_weights)
 
     stats = {}
-    stats["CAGR"] = round(CAGR(returns), 5)
-    stats["vol"] = round(np.std(returns)*np.sqrt(12), 5)
-    stats["Sharpe"] = round(CAGR(returns) / stats["vol"], 5)
-    stats["skew"] = round(float(skew(returns)), 5)
-    stats["maxDD"] = round(maxdrawdown(returns), 5)
-    return stats
+    stats["MVRR"] = returns
+    stats["vol"] = std
+    stats["Sharpe"] = stats["MVRR"] / stats["vol"]
+    stats["skew"] = return_value.apply(skew).values
+    stats["maxDD"] = return_value.apply(maxdrawdown).values
+    df_stats = pd.DataFrame(stats)
+    # returns = portfolio_value_half_yr_weight
+    return df_stats
 
-def get_MW_performance(portfolio_value, injective_capital, injection_cycle, injection_dates):
-    # use numpy.irr calculate the IRR which is the get_MW_performance
-    # outflow - inflow = 0
-    # -100,000 - sum(10,000/(1 + IRR)**i) + end_value/(1 + IRR)**n
-    # if injection_cycle == 6 months, IRR is compounding half-yearly
-    initial_outflow = portfolio_value[0]
-    ending_inflow = portfolio_value[-1]
+def get_weights(acc_value, injection_dates, before_injection_dates, Time_weighted = True):
+    portfolio_value_half_yr_weight = acc_value.iloc[acc_value.index.isin([acc_value.index[0]] + injection_dates)]
+    portfolio_value_half_yr_weight = portfolio_value_half_yr_weight.apply(lambda x: x/portfolio_value_half_yr_weight.sum(axis = 0),axis = 1).transpose().values
+    size = portfolio_value_half_yr_weight.shape[1] if portfolio_value_half_yr_weight.shape[1] %2 == 0 else portfolio_value_half_yr_weight.shape[1]-1
+    if Time_weighted == True:
+        return np.array([[1/size] * size] * portfolio_value_half_yr_weight.shape[0])
+    if portfolio_value_half_yr_weight.shape[1]%2 != 0:
+        portfolio_value_half_yr_weight = np.delete(portfolio_value_half_yr_weight, -1, axis=1)
+    return portfolio_value_half_yr_weight
 
-    cash_flow = [-initial_outflow]
-    cash_flow.extend([-injective_capital] * len(injection_dates))
-    cash_flow.append(ending_inflow)
+def get_returns(acc_value, injection_dates, before_injection_dates,weights):
+    portfolio_value_half_yr = acc_value.iloc[acc_value.index.isin([acc_value.index[0]] + before_injection_dates + injection_dates)].sort_index()
+    index = portfolio_value_half_yr.index
+    portfolio_value_half_yr = portfolio_value_half_yr.reset_index(drop = True)
+    if portfolio_value_half_yr.shape[0]%2 != 0:
+        portfolio_value_half_yr.drop(portfolio_value_half_yr.tail(1).index,inplace=True)
+    print(portfolio_value_half_yr)
+    semi_annually_return = portfolio_value_half_yr.groupby(portfolio_value_half_yr.index // 2).apply(lambda x: (x.iloc[1] - x.iloc[0]) / x.iloc[0]).values
+    returns = np.diag(weights.dot(semi_annually_return))
+    vfunc_rt = np.vectorize(lambda x: (1 + x / 2)**2 - 1)
+    returns = vfunc_rt(returns)
+    return index, returns
 
-    compounding_return = numpy.irr(cash_flow)
-    yearly_return = round((1 + compounding_return)**(12/injection_cycle),5)
-    return yearly_return
+def get_vol(return_value, index, weights):
+    breaked_vol = return_value.groupby(pd.cut(return_value.index, index)).apply(lambda x: np.std(x)).dropna().values
+    std = np.diag(weights.dot(breaked_vol))
+    vfunc_std = np.vectorize(lambda x: x * np.sqrt(2))
+    std = vfunc_std(std)
+    return std
