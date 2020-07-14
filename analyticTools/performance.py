90f12e348f0e8e4d6b5a1729f94b9c65fe63cbe9
Zale-1516 <65134434+Zale-1516@users.noreply.github.com>
2020-07-14 11:11:25 -0400

    push main and performance

--- analyticTools/performance.py
+++ analyticTools/performance.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.stats import skew
 import pandas as pd
 
+
 def maxdrawdown(timeseries):
     i = np.argmax(np.maximum.accumulate(timeseries) - timeseries) # end of the period
     if i == 0:
@@ -9,67 +10,46 @@ def maxdrawdown(timeseries):
     j = np.argmax(timeseries[:i]) # start of period
     return timeseries[i] / timeseries[j] - 1
 
-
-# def get_TW_performance(returns, risk_free = None):
-#
-#     def CAGR(x):
-#         return np.cumprod(1 + x)[-1]**(252/len(x)) - 1
-#
-#     price = np.cumprod(1+returns)
-#     print(price)
-#     risk_free = (risk_free.groupby(risk_free.index.year).mean()).values
-#     returns = ((1 + returns).groupby(returns.index.year).prod() - 1).values
-#     print(returns)
-#     assert len(returns) == len(risk_free)
-#
-#     stats = {}
-#     stats["CAGR"] = round(CAGR(returns), 5)
-#     stats["vol"] = round(np.std(returns)*np.sqrt(12), 5)
-#     stats["Sharpe"] = round(CAGR(returns) / stats["vol"], 5)
-#     stats["skew"] = round(float(skew(returns)), 5)
-#     stats["maxDD"] = round(maxdrawdown(returns), 5)
-#     return stats
-
-def get_performance(return_value, acc_value, injection_dates, before_injection_dates, Time_weighted = True):
+def get_performance(return_value, acc_value, injection_dates, before_injection_dates, risk_free_rate, std_risk_free, Time_weighted = True):
 
     portfolio_weights = get_weights(acc_value, injection_dates, before_injection_dates, Time_weighted)
     index, returns = get_returns(acc_value, injection_dates, before_injection_dates, portfolio_weights)
-    std = get_vol(return_value, index, portfolio_weights)
+    std = get_vol(return_value, index, portfolio_weights,isinstance(return_value, pd.Series))
 
     stats = {}
-    stats["MVRR"] = returns
+    stats["MVRR"] = returns - risk_free_rate
     stats["vol"] = std
     stats["Sharpe"] = stats["MVRR"] / stats["vol"]
-    stats["skew"] = return_value.apply(skew).values
-    stats["maxDD"] = return_value.apply(maxdrawdown).values
+    if isinstance(return_value, pd.Series) == False:
+        stats["skew"] = return_value.apply(skew).values
+        stats["maxDD"] = return_value.apply(maxdrawdown).values
     df_stats = pd.DataFrame(stats)
-    # returns = portfolio_value_half_yr_weight
     return df_stats
 
 def get_weights(acc_value, injection_dates, before_injection_dates, Time_weighted = True):
     portfolio_value_half_yr_weight = acc_value.iloc[acc_value.index.isin([acc_value.index[0]] + injection_dates)]
     portfolio_value_half_yr_weight = portfolio_value_half_yr_weight.apply(lambda x: x/portfolio_value_half_yr_weight.sum(axis = 0),axis = 1).transpose().values
-    size = portfolio_value_half_yr_weight.shape[1]-1
+    size = portfolio_value_half_yr_weight.shape[1]
     if Time_weighted == True:
         return np.array([[1/size] * size] * portfolio_value_half_yr_weight.shape[0])
-    portfolio_value_half_yr_weight = np.delete(portfolio_value_half_yr_weight, -1, axis=1)
     return portfolio_value_half_yr_weight
 
 def get_returns(acc_value, injection_dates, before_injection_dates,weights):
-    portfolio_value_half_yr = acc_value.iloc[acc_value.index.isin([acc_value.index[0]] + before_injection_dates + injection_dates)].sort_index()
+    portfolio_value_half_yr = acc_value.iloc[acc_value.index.isin([acc_value.index[0]] + before_injection_dates + injection_dates + [acc_value.index[-1]])].sort_index()
     index = portfolio_value_half_yr.index
     portfolio_value_half_yr = portfolio_value_half_yr.reset_index(drop = True)
-    if portfolio_value_half_yr.shape[0]%2 != 0:
-        portfolio_value_half_yr.drop(portfolio_value_half_yr.tail(1).index,inplace=True)
     semi_annually_return = portfolio_value_half_yr.groupby(portfolio_value_half_yr.index // 2).apply(lambda x: (x.iloc[1] - x.iloc[0]) / x.iloc[0]).values
     returns = np.diag(weights.dot(semi_annually_return))
-    vfunc_rt = np.vectorize(lambda x: (1 + x / 2)**2 - 1)
-    returns = vfunc_rt(returns)
+    returns = returns * 2
     return index, returns
 
-def get_vol(return_value, index, weights):
-    breaked_vol = return_value.groupby(pd.cut(return_value.index, index)).apply(lambda x: np.std(x)).dropna().values
-    std = np.diag(weights.dot(breaked_vol))
-    vfunc_std = np.vectorize(lambda x: x * np.sqrt(2))
-    std = vfunc_std(std)
+def get_vol(return_value, index, weights, is_series):
+
+    breaked_vol = return_value.groupby(pd.cut(return_value.index, index)).apply(lambda x: np.std(x)).replace(0, np.nan).dropna().values
+    breaked_vol = breaked_vol*np.sqrt(252)
+
+    if is_series == True:
+        breaked_vol = breaked_vol.reshape(breaked_vol.shape[0],1)
+    std = np.diag(np.sqrt(np.square(weights).dot(np.square(breaked_vol))))
+
     return std
--- main.py
+++ main.py
@@ -18,10 +18,14 @@ import time
 import pandas as pd
 from copulas.multivariate import GaussianMultivariate
 from copulas.visualization import scatter_2d
-# from pypfopt import EfficientFrontier
-# from pypfopt import risk_models
-# from pypfopt import expected_returns
-# from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+from pypfopt import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+
+import sys
+
+path = sys.path[0] + "/"
 
 def _get_tickers():
     if CLIENTS_INFO["risk_level"] == "high":
@@ -34,6 +38,13 @@ def _get_tickers():
 
 def nearest(items, pivot):
     return min(items, key=lambda x: abs(x - pivot))
+
+def get_last_date(dataframe, target):
+    date_list = list(dataframe.index)
+    for data in dataframe.index:
+        if data >= target:
+            index_i = date_list.index(data)
+            return dataframe.iloc[index_i]
 
 def leverage_return(r, rf, leverage_ratio):
     return r + leverage_ratio * (r - rf)
@@ -74,7 +85,7 @@ if __name__ == "__main__":
     ASSET_UNI_USD_Credit = ["IEF", "HYG", "LQD"]
     ASSET_UNI_CAD_Credit = ["XGB.TO", "XHY.TO", "XIG.TO"]
     ASSET_UNI_CAD_Equity = ["XIU.TO", "XIN.TO", "XEM.TO"]
-    ASSET_UNI_USD_Equity = ["VTI", "EFA", "VWO"] 
+    ASSET_UNI_USD_Equity = ["VTI", "EFA", "VWO"]
     RF_TICKER = ["^IRX"]
 
     # injection frequency = half year
@@ -85,11 +96,12 @@ if __name__ == "__main__":
     # backtest period: 2010.4.1 - 2015.03.31
     start_date = '2014-10-01'
     end_date = '2020-05-31'
+    first_test_start_date = '2015-04-01'
 
     #load data
-    df = yf.download(ASSET_UNI_USD_Credit + ASSET_UNI_CAD_Credit + ASSET_UNI_CAD_Equity + ASSET_UNI_USD_Equity + RF_TICKER, start=start_date, end=end_date)      
+    df = yf.download(ASSET_UNI_USD_Credit + ASSET_UNI_CAD_Credit + ASSET_UNI_CAD_Equity + ASSET_UNI_USD_Equity + RF_TICKER, start=start_date, end=end_date)
     df = df['Adj Close']
-    df["^IRX"] = df["^IRX"] / 100 
+    df["^IRX"] = df["^IRX"] / 100
     dt_list = list(df.index)
 
     # FX
@@ -102,6 +114,37 @@ if __name__ == "__main__":
 
     # fill na (forward)
     df.fillna(method = 'ffill', axis=0, inplace=True) #forward fill
+
+    """
+    benchmark start here --Zale
+    """
+    # path = r"C:\Users\11708\Documents\GitHub\Robo-Advisor"
+    # benchmark
+    benchmark_df = pd.read_csv(path + "\BlackRock.csv").set_index('Date')
+    benchmark_df.index = pd.to_datetime(benchmark_df.index)
+    cut_range = [x for x in benchmark_df.index if x >= pd.to_datetime(first_test_start_date) and x <= pd.to_datetime(end_date)]
+    benchmark_df = benchmark_df.loc[cut_range]
+    index = benchmark_df.index
+    benchmark_df = (benchmark_df['BlackRock 60/40'].pct_change() + 1).dropna()
+    benchmark_df = pd.DataFrame(np.insert(np.cumprod(benchmark_df.values.reshape(benchmark_df.values.shape[0])),0,1),index = index)
+
+    # benchmark_value initialize
+    portfolio_benchmark = [100000/get_last_date(df_fx,pd.to_datetime(first_test_start_date))] * benchmark_df.values.shape[0]
+    portfolio_benchmark = pd.DataFrame(portfolio_benchmark,index = index)
+    # print(benchmark_df)
+
+    risk_free_df = pd.read_csv(path + "\LIBOR_OVERNIGHT.csv").set_index('DATE')
+    risk_free_df.index = pd.to_datetime(risk_free_df.index)
+    cut_df = [x for x in risk_free_df.index if x >= pd.to_datetime(first_test_start_date) and x <= pd.to_datetime(end_date)]
+    risk_free_df = risk_free_df.loc[cut_df]
+    std_risk_free = np.std(risk_free_df).values[0]/np.sqrt(risk_free_df.values.shape[0])
+    risk_free_df = risk_free_df/100 + 1
+    index_risk_free = risk_free_df.index
+    risk_free_df = np.cumprod(risk_free_df.values.reshape(risk_free_df.values.shape[0]))
+    risk_free = risk_free_df[-1]**(1/risk_free_df.shape[0]) - 1
+    """
+    benchmark end here --Zale
+    """
 
     # rolling window & rebalance frequency (both in months)
     rolling_window = 6
@@ -133,7 +176,7 @@ if __name__ == "__main__":
     timeseriesUSDEquity = df_period_train[ASSET_UNI_USD_Equity].values
     timeseriesCADCredit = df_period_train[ASSET_UNI_CAD_Credit].values
     timeseriesCADEquity = df_period_train[ASSET_UNI_CAD_Equity].values
-    
+
     # portfolio optimize
     erc_rp = ERCRiskParity()
     # mvoUSD = MVPort(timeseriesUSD)
@@ -145,27 +188,27 @@ if __name__ == "__main__":
     risk_target_percent = [1 / timeseriesUSDEquity.shape[1]] * timeseriesUSDEquity.shape[1]
     weightUSDEquity = erc_rp.get_signal(timeseriesUSDEquity, initial_weights, risk_target_percent, False)
     weightUSDEquity *= 0.6
-    
+
     initial_weights = [1 / timeseriesUSDCredit.shape[1]] * timeseriesUSDCredit.shape[1]
     risk_target_percent = [1 / timeseriesUSDCredit.shape[1]] * timeseriesUSDCredit.shape[1]
     weightUSDCredit = erc_rp.get_signal(timeseriesUSDCredit, initial_weights, risk_target_percent, False)
     weightUSDCredit *= 0.4
-    
+
     initial_weights = [1 / timeseriesCADEquity.shape[1]] * timeseriesCADEquity.shape[1]
     risk_target_percent = [1 / timeseriesCADEquity.shape[1]] * timeseriesCADEquity.shape[1]
     weightCADEquity = erc_rp.get_signal(timeseriesCADEquity, initial_weights, risk_target_percent, False)
     weightCADEquity *= 0.6
-    
+
     initial_weights = [1 / timeseriesCADCredit.shape[1]] * timeseriesCADCredit.shape[1]
     risk_target_percent = [1 / timeseriesCADCredit.shape[1]] * timeseriesCADCredit.shape[1]
     weightCADCredit = erc_rp.get_signal(timeseriesCADCredit, initial_weights, risk_target_percent, False)
     weightCADCredit *= 0.4
-    
+
     # array to dict
     portfolio_USD = {**dict(zip(ASSET_UNI_USD_Credit, weightUSDCredit)), **dict(zip(ASSET_UNI_USD_Equity, weightUSDEquity))}
     portfolio_CAD = {**dict(zip(ASSET_UNI_CAD_Credit, weightCADCredit)), **dict(zip(ASSET_UNI_CAD_Equity, weightCADEquity))}
-    
-    # initialize two accounts  
+
+    # initialize two accounts
     # substract the transaction cost
     accountUSD = Account("USD", 50000 / df_fx.loc[test_start_date] - len(portfolio_USD) * 5, portfolio_USD)
     accountCAD = Account("CAD", 50000 - len(portfolio_CAD) * 5, portfolio_CAD)
@@ -175,7 +218,7 @@ if __name__ == "__main__":
     before_injection_dates = []
 
     for i in range(n_rebalance_freq):
-        
+
         price_arr_dict_CAD = df_period_test[ASSET_UNI_CAD_Credit + ASSET_UNI_CAD_Equity].to_dict('list')
         price_arr_dict_USD = df_period_test[ASSET_UNI_USD_Credit + ASSET_UNI_USD_Equity].to_dict('list')
 
@@ -191,11 +234,11 @@ if __name__ == "__main__":
 
             acc_CAD_val_list.append(v1)
             acc_USD_val_list.append(v2)
-        
+
             v2_cad = v2 * FX_arr[j]
-            
+
             adjustment = breach_check(v1, v2_cad)
-            
+
             if adjustment:
                 print("v1: "+ str(v1))
                 print("v2: "+ str(v2_ad))
@@ -241,27 +284,27 @@ if __name__ == "__main__":
         risk_target_percent = [1 / timeseriesUSDEquity.shape[1]] * timeseriesUSDEquity.shape[1]
         weightUSDEquity = erc_rp.get_signal(timeseriesUSDEquity, initial_weights, risk_target_percent, False)
         weightUSDEquity *= 0.6
-        
+
         initial_weights = [1 / timeseriesUSDCredit.shape[1]] * timeseriesUSDCredit.shape[1]
         risk_target_percent = [1 / timeseriesUSDCredit.shape[1]] * timeseriesUSDCredit.shape[1]
         weightUSDCredit = erc_rp.get_signal(timeseriesUSDCredit, initial_weights, risk_target_percent, False)
         weightUSDCredit *= 0.4
-        
+
         initial_weights = [1 / timeseriesCADEquity.shape[1]] * timeseriesCADEquity.shape[1]
         risk_target_percent = [1 / timeseriesCADEquity.shape[1]] * timeseriesCADEquity.shape[1]
         weightCADEquity = erc_rp.get_signal(timeseriesCADEquity, initial_weights, risk_target_percent, False)
         weightCADEquity *= 0.6
-        
+
         initial_weights = [1 / timeseriesCADCredit.shape[1]] * timeseriesCADCredit.shape[1]
         risk_target_percent = [1 / timeseriesCADCredit.shape[1]] * timeseriesCADCredit.shape[1]
         weightCADCredit = erc_rp.get_signal(timeseriesCADCredit, initial_weights, risk_target_percent, False)
         weightCADCredit *= 0.4
-        
+
         # array to dict
         portfolio_USD = {**dict(zip(ASSET_UNI_USD_Credit, weightUSDCredit)), **dict(zip(ASSET_UNI_USD_Equity, weightUSDEquity))}
         portfolio_CAD = {**dict(zip(ASSET_UNI_CAD_Credit, weightCADCredit)), **dict(zip(ASSET_UNI_CAD_Equity, weightCADEquity))}
         #weightCAD = initial_weights
-        
+
         # previous portfolio weight
         pre_weight_CAD = accountCAD.get_weight()
         pre_weight_USD = accountUSD.get_weight()
@@ -269,7 +312,7 @@ if __name__ == "__main__":
         # calculate t cost for each turnover
         portf_USD_diff = {key: abs(pre_weight_USD[key] - portfolio_USD[key]) for key in portfolio_USD.keys()}
         portf_CAD_diff = {key: abs(pre_weight_CAD[key] - portfolio_CAD[key]) for key in portfolio_CAD.keys()}
-        
+
         temp1 = sum(portf_USD_diff.values())
         temp2 = sum(portf_CAD_diff.values())
         t_cost_usd = len(temp1[temp1>0.001]) * 5
@@ -292,6 +335,29 @@ if __name__ == "__main__":
         if i % (INJECTION_FREQ / rebalance_freq) == 0:
             before_injection_dates.append(test_end_date)
 
+    """
+    Benchmark continue here --Zale
+    """
+
+    for injection_date in injection_dates:
+        first = True
+        for i in portfolio_benchmark.index:
+            if i >= injection_date and first == True:
+                convert_rate_i = df_fx.loc[i]
+            if i >= injection_date:
+                portfolio_benchmark.loc[i] = 10000/convert_rate_i + portfolio_benchmark.loc[i]
+                first = False
+
+    fx_benchmark = df_fx.iloc[df_fx.index.isin(portfolio_benchmark.index)]
+    portfolio_benchmark = portfolio_benchmark.iloc[portfolio_benchmark.index.isin(fx_benchmark.index)]
+    benchmark_df = benchmark_df.iloc[benchmark_df.index.isin(fx_benchmark.index)]
+    size = fx_benchmark.values.shape[0]
+    benchmark_cad = portfolio_benchmark.values.reshape(size) * fx_benchmark.values.reshape(size) * benchmark_df.values.reshape(size)
+    benchmark_cad = pd.DataFrame(benchmark_cad,index = portfolio_benchmark.index)
+
+    """
+    Benchmark end here --Zale
+    """
 
     test_time_period = dt_list[_test_start_date_index:]
 
@@ -302,49 +368,53 @@ if __name__ == "__main__":
     acc_value_df['portfolio'] = acc_value_df['accountUSD_CADHDG'] + acc_value_df['accountCAD']
 
 
-    
+
 #%%
 
     # calculate daily return
 
     acc_ret_df_d = splitted_returns(acc_value_df, injection_dates)
-    
-    # calculate quartly return
-    acc_ret_df_q = acc_value_df.groupby(pd.PeriodIndex(acc_value_df.index, freq='Q'), axis=0).apply(lambda x: (x.iloc[-1] - x.iloc[0]) / x.iloc[0])
-    
-    
-    
-#%% calculate daily leveraged return 
-     
+
+    # calculate daily leveraged return
+
     # read libor overnight rate
-    Libor_overnight = pd.read_csv("C:\\Users\\zhong\\Documents\\MMF\\Risk Management Lab\\Robo-Advisor\\data\\LIBOR_OVERNIGHT.csv", header=0)
-    Libor_overnight.index = Libor_overnight["DATE"]
-    del Libor_overnight["DATE"]
-    Libor_overnight.replace('.', np.nan, inplace=True)
-    Libor_overnight.fillna(method='ffill', inplace=True, axis=0)
-    Libor_overnight = Libor_overnight.astype(float)
-    Libor_overnight = Libor_overnight.groupby(pd.PeriodIndex(Libor_overnight.index, freq='Q'), axis=0).first()
-    
+    # Libor_overnight = pd.read_csv("C:\\Users\\zhong\\Documents\\MMF\\Risk Management Lab\\Robo-Advisor\\data\\LIBOR_OVERNIGHT.csv", header=0)
+    # Libor_overnight.index = Libor_overnight["DATE"]
+    # del Libor_overnight["DATE"]
+    # Libor_overnight.replace('.', np.nan, inplace=True)
+    # Libor_overnight.fillna(method='ffill', inplace=True, axis=0)
+    # Libor_overnight = Libor_overnight.astype(float)
+    # Libor_overnight = Libor_overnight.groupby(pd.PeriodIndex(Libor_overnight.index, freq='Q'), axis=0).first()
+
     # acc_ret_df_d = acc_ret_df_d.join(Libor_overnight)
     # leverage_ratio = 0.2
     # acc_ret_df_d['lev_portf_ret'] = acc_ret_df_d['portfolio'] + leverage_ratio * (acc_ret_df_d['portfolio'] - acc_ret_df_d['LIBOR_OVERNIGHT'] / 100)
 
     # try get_performance
-    print(get_performance(acc_ret_df_d, acc_value_df, injection_dates, before_injection_dates, True))
-    
+    acc_value_df.to_csv(path + "\Acc_value_df.csv", index=True)
+    acc_ret_df_d.to_csv(path + "\Acc_ret_df_d.csv", index=True)
+    benchmark_cad.to_csv(path + "\Benchamark_value.csv", index = True)
+    get_performance(acc_ret_df_d, acc_value_df, injection_dates, before_injection_dates, risk_free, std_risk_free, False).to_csv(path + "\portfolio_performance.csv", index=True)
+    get_performance(fx_benchmark, benchmark_cad, injection_dates, before_injection_dates, risk_free, std_risk_free, False).to_csv(path + "\Benchmark_performance.csv", index=True)
+
+    print(get_performance(acc_ret_df_d, acc_value_df, injection_dates, before_injection_dates, risk_free, std_risk_free, False))
+    print(get_performance(fx_benchmark, benchmark_cad, injection_dates, before_injection_dates, risk_free, std_risk_free, False))
+    # calculate quartly return
+    acc_ret_df_q = acc_value_df.groupby(pd.PeriodIndex(acc_value_df.index, freq='Q'), axis=0).apply(lambda x: (x.iloc[-1] - x.iloc[0]) / x.iloc[0])
+
     #calculate leveraged quarterly return
     acc_ret_df_q = acc_ret_df_q.join(Libor_overnight)
     leverage_ratio = 0.2
     acc_ret_df_q['lev_portf_ret'] = acc_ret_df_q['portfolio'] + leverage_ratio * (acc_ret_df_q['portfolio'] - acc_ret_df_q['LIBOR_OVERNIGHT'] / 100)
-    
+
     acc_value_df.plot()
     plt.legend()
     plt.show()
-    
+
 #%%
     # factor analysis
 
-    factor_df = pd.read_excel('C:\\Users\\zhong\\Documents\\MMF\\Risk Management Lab\\Robo-Advisor\\data\\factor data.xlsx', header=0)
+    factor_df = pd.read_excel(path + '\factor data.xlsx', header=0)
     factor_df.index = factor_df['DATE']
     del factor_df['DATE']
 
